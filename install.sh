d26b4b9674f2096714c02fe831af977346d64684
Handgun5501 <git@nihilum.xyz>
2025-03-25 19:05:32 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,73 @@
+#updating:
+echo "updating debian"
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove
+#installing xfce
+echo "installing xfce"
+sudo apt install xfce4
+sudo apt install \
+    libxfce4ui-utils \
+    thunar \
+    xfce4-appfinder \
+    xfce4-panel \
+    xfce4-session \
+    xfce4-settings \
+    xfce4-terminal \
+    xfconf \
+    xfdesktop4 \
+    xfwm4
+#install git
+echo "installing git"
+sudo apt install git
+
+#installing web browser
+echo "installing mullvad browser"
+# Download the Mullvad signing key
+sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+
+# Add the Mullvad repository server to apt
+echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
+
+# Install the package
+sudo apt update
+sudo apt install mullvad-browser
+
+
+#installing signal
+echo "installing signal"
+# NOTE: These instructions only work for 64-bit Debian-based
+# Linux distributions such as Ubuntu, Mint etc.
+
+# 1. Install our official public software signing key:
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
+# 2. Add our repository to your list of repositories:
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+
+# 3. Update your package database and install Signal:
+sudo apt update && sudo apt install signal-desktop
+
+#install flathub
+echo "flathub + bitwarden install"
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+echo "reboot when next command does not work"
+flatpak install flathub com.bitwarden.desktop
+
+#tuta install
+https://app.tuta.com/desktop/tutanota-desktop-linux.AppImage
+
+#unattended upgrades
+echo "auto update debian"
+sudo apt update
+sudo apt install unattended-upgrades
+sudo dpkg-reconfigure --priority=low unattended-upgrades
+sudo systemctl enable unattended-upgrades
+
+echo "done"
+
+sudo reboot
