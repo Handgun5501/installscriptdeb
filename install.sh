5023c2810d08d7c8c8928532760640bc86e9efcf
Handgun5501 <git@nihilum.xyz>
2025-03-25 23:16:36 +0000

    just trying out some bash scripting
    (i have no idea what i am doing and i am a script kiddie right now).

--- install.sh
+++ install.sh
@@ -1,10 +1,46 @@
+echo "WARNING I AM NOT A CODER AND MADE THIS FOR MYSELF STILL WANT TO USE THE SCRIPT?"
+echo "NO WARRENTY NO SUPPORT NOTHING YOU ARE RESPONSIBLE"
+echo "THEN TYPE YES"
+echo "ELSE DO CONTROL Z OR CLOSE TERMINAL"
+read x
+if [ "$x" = "yes"]
+then
+echo "brave soul, starting script now. You have be warned!"
+fi
+
+
+
+
+
 #updating:
-echo "updating debian"
+echo "forced update"
 sudo apt update
 sudo apt upgrade
-sudo apt autoremove
+#install git
+
+echo "INSTRUCTIONS"
+echo "type yes to install type no or press enter to skip"
+
+echo want to install git and curl?
+read x
+if [ "$x" = "yes" ]
+then
+  # do the dangerous stuff
+echo "installing git"
+sudo apt install git
+echo "install curl"
+sudo apt install curl
+else
+echo "curl and git not installed, installer may  not work!"
+fi
+
+
 #installing xfce
-echo "installing xfce"
+echo want to install xfce?
+echo "WITHOUT XFCE YOU WILL NOT HAVE A DESKTOP"
+read x
+if [ "$x" = "yes" ]
+then
 sudo apt install xfce4
 sudo apt install \
     libxfce4ui-utils \
@@ -17,16 +53,14 @@ sudo apt install \
     xfconf \
     xfdesktop4 \
     xfwm4
-#install git
-echo "installing git"
-sudo apt install git
-echo "install curl"
-sudo apt install curl
+else 
+echo "no desktop! user cancelled"
+fi
 #installing web browser
-echo "installing mullvad browser"
-
-
-
+echo "want to install mullvad browser?"
+read x
+if [ "$x" = "yes" ]
+then
 # Download the Mullvad signing key
 sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
 
@@ -36,19 +70,21 @@ echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --prin
 # Install the package
 sudo apt update
 sudo apt install mullvad-browser
-# And to install the alpha version
-sudo apt install mullvad-browser-alpha
+
+else
+echo "no browser i hope that you know what you are doing...."
+fi
 
 
 
-
-
-
+echo "want to install signal desktop?"
+read x
+if [ "$x" = "yes" ]
+then
 #installing signal
 echo "installing signal"
 # NOTE: These instructions only work for 64-bit Debian-based
 # Linux distributions such as Ubuntu, Mint etc.
-
 # 1. Install our official public software signing key:
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
 cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
@@ -59,23 +95,35 @@ echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] 
 
 # 3. Update your package database and install Signal:
 sudo apt update && sudo apt install signal-desktop
+else
+echo "signal desktop not installed"
+fi
 
-#install flathub
-echo "flathub + bitwarden install"
+
+echo "want to install bitwarden?"
+echo "flathub is needed and will be installed"
+read x
+if [ "$x" = "yes" ]
+then
 sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-echo "reboot when next command does not work"
 flatpak install flathub com.bitwarden.desktop
-
+else
+echo "bitwarden and flathub not installed"
+fi
 #tuta install
-https://app.tuta.com/desktop/tutanota-desktop-linux.AppImage
 
-#unattended upgrades
-echo "auto update debian"
-sudo apt update
-sudo apt install unattended-upgrades
-sudo dpkg-reconfigure --priority=low unattended-upgrades
-sudo systemctl enable unattended-upgrades
+echo "want to install tuta mail appimage?"
+read x
+if [ "$x" = "yes" ]
+then
+sudo wget https://app.tuta.com/desktop/tutanota-desktop-linux.AppImage
+else
+echo "tuta not installed"\
+fi
 
-echo "done"
+fi
+
+
+echo "THANKS FOR USING MY SCRIPT"
+
